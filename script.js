148016e1a78e15795403303374335f90ffa280ba
fongitex <fongitex@gmail.com>
2024-11-25 17:55:47 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+﻿function calculate() {
+    const charge = parseFloat(document.getElementById("charge").value);
+    const testType = document.getElementById("testType").value;
+    let result = "";
+
+    if (isNaN(charge) || charge <= 0) {
+        result = "Veuillez entrer une charge admissible valide.";
+    } else {
+        if (charge < 5) {
+            if (testType === "statique") {
+                result = "Valeur d'essai statique : " + (charge * 1.33).toFixed(2) + " tonnes.";
+            } else {
+                result = "Valeur d'essai dynamique : " + (charge * 1.1).toFixed(2) + " tonnes.";
+            }
+        } else {
+            if (testType === "statique") {
+                result = "Valeur d'essai statique : " + (charge * 1.5).toFixed(2) + " tonnes.";
+            } else {
+                result = "Valeur d'essai dynamique : " + (charge * 1.2).toFixed(2) + " tonnes.";
+            }
+        }
+    }
+
+    document.getElementById("result").innerText = result;
+}
